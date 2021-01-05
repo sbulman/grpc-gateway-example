e338d3357dcedc6f0ffc282e49c2a3922131b8fc
Simon Bulman <SBulman@slb.com>
2021-01-05 09:59:59 +0000

    Define grpc API

--- generate.go
+++ generate.go
@@ -1,3 +1,5 @@
 // +build tools
 
 package tools
+
+//go:generate protoc --proto_path=api/proto/v1 --proto_path=third_party --go_out=. --go_opt=module=github.com/sbulman --go-grpc_out=. --go-grpc_opt=module=github.com/sbulman todo_service.proto
